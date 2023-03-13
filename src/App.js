30278cdfc5c89a91bad7b21479303b00a4304db1
Raphael Molinaro <64808456+rmc3408@users.noreply.github.com>
2023-03-13 16:33:30 -0400

    fix: semantic and workflow folder
    * creating develop workflow to push (#4)
    
    * env file
    
    * event name
    
    * new
    
    * fixed
    
    * secret
    
    * issue yaml
    
    * fix1
    
    * new file
    
    * fix file
    
    * master branch
    
    * init
    
    * 2 lines
    
    * main added
    
    * add
    
    * hard coded
    
    * premission
    
    * action push
    
    * main
    
    * toekn
    
    * removed
    
    * more permission
    
    * random-created
    
    * decrypt
    
    * one line
    
    * typo
    
    * pushFile
    
    * random-created
    
    * context
    
    * function
    
    * failure
    
    * type job
    
    * matrix
    
    * typo setunode
    
    * exclude
    
    * mongo
    
    * typo post
    
    * one line
    
    * quotes
    
    * no quotes end
    
    * alialla
    
    * json post
    
    * single quotes
    
    * docker
    
    * 2 func
    
    * node
    
    * echos
    
    * bar
    
    * chmod
    
    * name fixed
    
    * chmod
    
    * script typo
    
    * test
    
    * sh
    
    * fix echo
    
    * section 4 done
    
    * react app created
    
    * rearranging section 5
    
    * owners
    
    * owner test in main
    
    * code owner test main admins
    
    * first try workflow 01
    
    * adding package-lock error
    
    * cleaning format:write
    
    * X added
    
    * missing T
    
    * passing JEST test
    
    * develop push workflow 1
    
    * new text
    
    * env file
    
    * event name
    
    * new
    
    * fixed
    
    * secret
    
    * issue yaml
    
    * fix1
    
    * new file
    
    * fix file
    
    * master branch
    
    * init
    
    * 2 lines
    
    * main added
    
    * add
    
    * hard coded
    
    * premission
    
    * action push
    
    * main
    
    * toekn
    
    * removed
    
    * more permission
    
    * random-created
    
    * decrypt
    
    * one line
    
    * typo
    
    * pushFile
    
    * random-created
    
    * context
    
    * function
    
    * failure
    
    * type job
    
    * matrix
    
    * typo setunode
    
    * exclude
    
    * mongo
    
    * typo post
    
    * one line
    
    * quotes
    
    * no quotes end
    
    * alialla
    
    * json post
    
    * single quotes
    
    * docker
    
    * 2 func
    
    * node
    
    * echos
    
    * bar
    
    * chmod
    
    * name fixed
    
    * chmod
    
    * script typo
    
    * test
    
    * sh
    
    * fix echo
    
    * section 4 done
    
    * react app created
    
    * rearranging section 5
    
    * owners
    
    * owner test in main
    
    * code owner test main admins
    
    * first try workflow 01
    
    * adding package-lock error
    
    * cleaning format:write
    
    * X added
    
    * missing T
    
    * passing JEST test
    
    * develop push workflow 1
    
    * new text
    
    * Feature cache (#6)
    
    * env file
    
    * event name
    
    * new
    
    * fixed
    
    * secret
    
    * issue yaml
    
    * fix1
    
    * new file
    
    * fix file
    
    * master branch
    
    * init
    
    * 2 lines
    
    * main added
    
    * add
    
    * hard coded
    
    * premission
    
    * action push
    
    * main
    
    * toekn
    
    * removed
    
    * more permission
    
    * random-created
    
    * decrypt
    
    * one line
    
    * typo
    
    * pushFile
    
    * random-created
    
    * context
    
    * function
    
    * failure
    
    * type job
    
    * matrix
    
    * typo setunode
    
    * exclude
    
    * mongo
    
    * typo post
    
    * one line
    
    * quotes
    
    * no quotes end
    
    * alialla
    
    * json post
    
    * single quotes
    
    * docker
    
    * 2 func
    
    * node
    
    * echos
    
    * bar
    
    * chmod
    
    * name fixed
    
    * chmod
    
    * script typo
    
    * test
    
    * sh
    
    * fix echo
    
    * section 4 done
    
    * react app created
    
    * rearranging section 5
    
    * owners
    
    * owner test in main
    
    * code owner test main admins
    
    * first try workflow 01
    
    * adding package-lock error
    
    * cleaning format:write
    
    * X added
    
    * missing T
    
    * passing JEST test
    
    * develop push workflow 1
    
    * new text
    
    * caching
    
    * building artifact
    
    * directory upload artifact error fix
    
    * fix: fix semantic
    
    * testing release
    
    * feat: semantic
    
    * fix: delete workflow folder

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p> Built in development mode</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
