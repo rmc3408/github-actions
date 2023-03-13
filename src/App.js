b379abaf50cdbf4cde0c0b201a6e0993c1c7df30
Raphael <rmc3408@protonmail.com>
2023-03-13 07:35:32 -0400

    rearranging section 5

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const a = { namee: 1, age: 233, num: 234 };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,12 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
